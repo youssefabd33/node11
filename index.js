48928349629f0bbf06d225feaa5f5244f1698751
youssefabd01158 <161996935+youssefabd33@users.noreply.github.com>
2024-03-04 05:12:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+// imports
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser=require("body-parser").urlencoded({extended:true});
+const mongoose = require('mongoose');
 
+
+//DB CONNECTIONS
+const mongo_username = process.env['MONGO_USERNAME'];
+const mongo_password = process.env['MONGO_PASSWORD'];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@firstdb.a6xnaav.mongodb.net/?retryWrites=true&w=majority&appName=firstdb`;
+
+const dbname = 'deploy';
+const collectionName = 'users';
+
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email:String,
+  message:String
+
+});
+
+const User = mongoose.model(collectionName, userSchema, collectionName);
 // Serve static files
 app.use(express.static('public'));
 
@@ -9,6 +30,52 @@ app.use(express.static('public'));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
+//post middle ware
+
+app.post("/form",bodyParser,(req,res)=>{
+const username=req.body.name; 
+const email=req.body.email;
+const message=req.body.message;
+  mongoose.connect(uri, { dbName: dbname })
+  .then(() => {
+    const db = mongoose.connection.db;
+
+
+
+
+    const newUser = new User({
+      name:username ,
+      email:email,
+      message:message
+
+    });
+
+    newUser
+      .save()
+      .then(() => {
+        console.log("User saved successfully");
+        res.send("User saved successfully");
+      })
+      .catch((error) => {
+        console.error("Error saving user:", error);
+        res.send("Error saving user");
+      })
+      .finally(() => {
+        mongoose.connection.close(); // Close the connection after the operation is complete
+      });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+    res.send("Error connecting to MongoDB Atlas");
+  });
+
+
+})
+
+
+
+
 
 // Start the server
 app.listen(3000, () => {
