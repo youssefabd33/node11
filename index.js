e1d7de0c7317526bec7bbfe3b1d75ef87de7e9c3
youssefabd01158 <161996935+youssefabd33@users.noreply.github.com>
2024-03-06 00:41:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,81 +1,17 @@
-// imports
 const express = require('express');
 const app = express();
-const path = require('path');
-const bodyParser=require("body-parser").urlencoded({extended:true});
-const mongoose = require('mongoose');
+const route = require('./router/userRouter');
+const bodyParser = require("body-parser");
 
-
-//DB CONNECTIONS
-const mongo_username = process.env['MONGO_USERNAME'];
-const mongo_password = process.env['MONGO_PASSWORD'];
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@firstdb.a6xnaav.mongodb.net/?retryWrites=true&w=majority&appName=firstdb`;
-
-const dbname = 'deploy';
-const collectionName = 'users';
-
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  email:String,
-  message:String
-
-});
-
-const User = mongoose.model(collectionName, userSchema, collectionName);
 // Serve static files
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.get("/",route)
 
-//post middle ware
-
-app.post("/form",bodyParser,(req,res)=>{
-const username=req.body.name; 
-const email=req.body.email;
-const message=req.body.message;
-  mongoose.connect(uri, { dbName: dbname })
-  .then(() => {
-    const db = mongoose.connection.db;
-
-
-
-
-    const newUser = new User({
-      name:username ,
-      email:email,
-      message:message
-
-    });
-
-    newUser
-      .save()
-      .then(() => {
-        console.log("User saved successfully");
-       res.sendFile(path.join(__dirname, "thank.html"));
-      })
-      .catch((error) => {
-        console.error("Error saving user:", error);
-        res.send("Error saving user");
-      })
-      .finally(() => {
-        mongoose.connection.close(); // Close the connection after the operation is complete
-      });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB Atlas:", error);
-    res.send("Error connecting to MongoDB Atlas");
-  });
-
-
-})
-
-
-
-
+app.post("/form", route);
 
 // Start the server
 app.listen(3000, () => {
